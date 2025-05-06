d7f26db27cc0313d73a8352aef2e541756498d06
kancharlaniranjan <139963301+kancharlaniranjan@users.noreply.github.com>
2025-05-07 01:02:05 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+# Task 1
+a= int(input('Enter the First Number: '))
+b= int(input('Enter the Second Number: '))
+print('Addition: ',a+b)
+print('subtraction: ',a-b)
+print('Multiplication: ',a*b)
+print('Division: ',a/b)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+#Task 2
+c= input('Enter Your First Name: ')
+d= input('Enter your Last Name: ')
+e=c+d
+print('Hi,',e,'! How is your Day?')
