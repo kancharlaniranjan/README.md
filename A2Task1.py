e76435cefaa09c5d997b88c44d39e604adc426ac
kancharlaniranjan <139963301+kancharlaniranjan@users.noreply.github.com>
2025-05-12 22:37:28 +0530

    Add files via upload

---
+++ A2Task1.py
@@ -0,0 +1,6 @@
+#Defining user input value as Even or Odd
+a=int(input("Enter a Number:"))
+if (a%2)==0:
+    print(a,"is an Even Number")
+else:
+    print(a,"is an Odd Number")
---
+++ A2Task2.py
@@ -0,0 +1,5 @@
+# sum of 1 to 50
+a=0
+for z in range(1,51):
+    a=a+z
+print(a)
