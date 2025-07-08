70c57162381d86bf326cfa45165f137e4180ad3e
kancharlaniranjan <139963301+kancharlaniranjan@users.noreply.github.com>
2025-07-08 19:55:20 +0530

    Add files via upload

---
+++ A5Task1.py
@@ -0,0 +1,12 @@
+# Task - 1
+
+Stu_dict = {"Aleem":79,"Balu":83,"Charan":71,"Gangi":90,"Suresh":65}
+stu_name = input("Enter Stu Name:")
+try:
+    print(Stu_dict[stu_name])
+except:
+    print("student Not found")
+
+
+
+
---
+++ A5Task2.py
@@ -0,0 +1,8 @@
+# Task - 2
+
+a = [1,2,3,4,5,6,7,8,9,10]
+b = a[0:5]
+c = b[::-1]
+print("Original List:",a)
+print("Extracted first five Elements:",b)
+print("Reversed Extracted Elements:",c)
